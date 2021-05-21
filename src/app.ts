b39d0af4967870c6a3a3c9a7491ab80006750994
Rostyslav Makhanko <rostyslavm@wix.com>
2021-05-21 18:01:26 +0300

    Init ts packages

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send(
+    "Some day there will be cool API for for personal financial management application thesis!"
+  );
+});
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
